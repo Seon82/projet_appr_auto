1c84c4ad8790d56e980a5555076de2c7adf0650e
noblotni <noblotni@gmail.com>
2022-10-21 18:30:01 +0200

    Add a crossvalidation function.

--- airbnb_prices/eval/train_eval.py
+++ airbnb_prices/eval/train_eval.py
@@ -1,6 +1,9 @@
 import logging
+
+import numpy as np
 from pandas import DataFrame
 from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import KFold
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,4 +27,30 @@ def train_eval_once(
     y_pred = model.predict(x_test)
     score = mean_squared_error(y_test, y_pred)
     logging.info(" MSE score on test set: {}".format(score))
-    return model
+    return model, score
+
+
+def cross_validation(model, x: DataFrame, y: DataFrame):
+    """Perform a 5-fold crossvalidation with the model.
+
+    Args:
+        model : model to train and evaluate
+        x (DataFrame): _train set
+        y (DataFrame): _train set target_
+
+    Returns:
+        _score: crossvalidation score. It is the average of the
+            evaluation scores._
+    """
+    # list to keep track of the scores
+    cv_scores = []
+    # 5-fold crossvalidation
+    kfold = KFold(n_splits=5)
+    for train_index, test_index in kfold.split(x):
+        x_train, y_train = x[train_index], y[train_index]
+        x_test, y_test = x[test_index], y[test_index]
+        _, score = train_eval_once(
+            model=model, x_train=x_train, y_train=y_train, x_test=x_test, y_test=y_test
+        )
+        cv_scores.append(score)
+    return np.mean(cv_scores)
