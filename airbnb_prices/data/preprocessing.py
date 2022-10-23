1667ef26d914801a1ce1b736faaa1e3653fcf190
Seon82 <46298009+Seon82@users.noreply.github.com>
2022-10-23 12:45:29 +0200

    feat: more config options

--- airbnb_prices/data/preprocessing.py
+++ airbnb_prices/data/preprocessing.py
@@ -1,21 +1,33 @@
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.base import TransformerMixin
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 from airbnb_prices.config import PreprocessingConfig
 
 
 def apply_preprocessing(
-    df: pd.DataFrame, config: PreprocessingConfig, scalers=None | dict[str, StandardScaler]
+    df: pd.DataFrame, config: PreprocessingConfig, scalers: None | dict[str, TransformerMixin] = None
 ):
     for col in config.ohe:
         df = pd.concat((df, pd.get_dummies(df[col], prefix=col)), axis=1)
     if scalers is None:
         scalers = {}
     for col in config.standardize:
-        if col in scalers:
-            df[col] = scalers[col].transform(df[col])
-        else:
-            scalers[col] = StandardScaler()
-            df[col] = scalers[col].fit_transform(df[col])
-    df = df.drop(config.drop, axis=1)
+        df, scalers = apply_scaling(df, col, scalers, StandardScaler)
+    for col in config.min_max:
+        df, scalers = apply_scaling(df, col, scalers, MinMaxScaler)
+
+    if config.drop:
+        df = df.drop(config.drop, axis=1)
+    elif config.keep:
+        df = df.drop(df.columns.difference(config.keep), axis=1)
     return scalers, df
+
+
+def apply_scaling(df, col, scalers, scaler_type):
+    if col in scalers:
+        df[col] = scalers[col].transform(df[col].values.reshape(-1, 1))
+    else:
+        scalers[col] = scaler_type()
+        df[col] = scalers[col].fit_transform(df[col].values.reshape(-1, 1))
+    return df, scalers
